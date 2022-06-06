df9b226271e82ab40c4e1275f807520448f2dd99
Akhmet <golden-silver_777@mail.ru>
2022-06-06 10:16:52 +0500

    feat:config file database

---
+++ config/config.php
@@ -0,0 +1,10 @@
+<?php
+
+define("DB_DRIVER", "mysql");
+define("DB_HOST", "localhost");
+define("DB_USER", "root");
+define("DB_PASSWORD", "root");
+define("DB_DATABASE", "reservation");
+define("DB_CHARSET", "utf8");
+
+?>
