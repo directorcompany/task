b5d091a32c49cb362f1456ab418079fd295812fe
DirectorCompany <58828108+directorcompany@users.noreply.github.com>
2022-06-06 10:34:41 +0500

    fix: data

--- config/config.php
+++ config/config.php
@@ -2,8 +2,8 @@
 
 define("DB_DRIVER", "mysql");
 define("DB_HOST", "localhost");
-define("DB_USER", "root");
-define("DB_PASSWORD", "root");
+define("DB_USER", "root"); 
+define("DB_PASSWORD", ""); 
 define("DB_DATABASE", "reservation");
 define("DB_CHARSET", "utf8");
 
